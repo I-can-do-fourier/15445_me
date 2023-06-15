6d45bf96930fbeadbc263690bcbc5f0a31d89a20
hxh <noname98@126.com>
2023-06-15 15:44:48 +0800

    p1 task2.

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -3,13 +3,52 @@
 
 namespace bustub {
 
-BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {}
+BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
 
-void BasicPageGuard::Drop() {}
+ bpm_=that.bpm_;
+ is_dirty_=that.is_dirty_;
+ page_=that.page_;
 
-auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { return *this; }
+ that.bpm_= nullptr;
+ that.is_dirty_= false;
+ that.page_= nullptr;
 
-BasicPageGuard::~BasicPageGuard(){};  // NOLINT
+}
+
+void BasicPageGuard::Drop() {
+
+  if (page_ != nullptr&&bpm_!= nullptr) bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
+  bpm_= nullptr;
+  is_dirty_=false;
+  page_= nullptr
+
+}
+
+auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & {
+
+  BUSTUB_ASSERT(this != &that,"self assignment");
+
+
+  if (page_ != nullptr) bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
+
+  bpm_=that.bpm_;
+  is_dirty_=that.is_dirty_;
+  page_=that.page_;
+
+  that.bpm_= nullptr;
+  that.is_dirty_= false;
+  that.page_= nullptr;
+
+
+  return *this;
+
+
+}
+
+BasicPageGuard::~BasicPageGuard(){
+
+    Drop();
+};  // NOLINT
 
 ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default;
 
